6177acddd8a0e5362763c5e9dcd6065b26e157cf
SakagamiJun <973175+SakagamiJun@users.noreply.github.com>
2026-06-27 14:12:45 +0800

    feat: add panelneko cask template

---
+++ Casks/panelneko.rb
@@ -0,0 +1,24 @@
+cask "panelneko" do
+  version "0.1.0"
+  
+  on_arm do
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    url "https://github.com/SakagamiJun/panelneko-reader/releases/download/v#{version}/panelneko-reader-macos-arm64.zip"
+  end
+  on_intel do
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    url "https://github.com/SakagamiJun/panelneko-reader/releases/download/v#{version}/panelneko-reader-macos-amd64.zip"
+  end
+
+  name "PanelNeko"
+  desc "A beautiful and modern comic reader"
+  homepage "https://github.com/SakagamiJun/panelneko-reader"
+
+  app "panelneko-reader.app"
+
+  zap trash: [
+    "~/Library/Application Support/panelneko-reader",
+    "~/Library/Preferences/com.wails.panelneko-reader.plist",
+    "~/Library/Saved Application State/com.wails.panelneko-reader.savedState",
+  ]
+end
